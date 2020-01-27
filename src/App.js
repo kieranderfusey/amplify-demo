d9ccafb7ea8cb625b8c48f051c9b9fc5a8d71fb2
Kieran P Derfus <Kieran.P.Derfus@ey.com>
2020-01-27 14:37:40 -0500

    EDIT: Welcome to EY FSOt

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save.
+          Welcome to <code>EY FSOt</code>
         </p>
         <a
           className="App-link"
@@ -18,7 +18,6 @@ function App() {
         >
           Learn React
         </a>
-        <img src="https://picsum.photos/id/237/200/300" className="App-logo" alt="logo" />
       </header>
     </div>
   );
