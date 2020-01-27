1057298a8001df06f570139b01b47bfaa8415efc
Kieran P Derfus <Kieran.P.Derfus@ey.com>
2020-01-27 14:53:41 -0500

    EDIT: Demo configuration

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
+// import code from './code.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="code" />
         <p>
-          Welcome to <code>EY FSOt</code>
+          Welcome to <code>React</code>
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
+          // href="https://www.ey.com/en_gl"
           target="_blank"
           rel="noopener noreferrer"
         >
