19de67aaa115acc569145abb64c3244b18863cd6
Kieran P Derfus <Kieran.P.Derfus@ey.com>
2020-01-27 14:27:54 -0500

    ADD: Additional photo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save.
         </p>
         <a
           className="App-link"
@@ -18,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <img src="https://picsum.photos/id/237/200/300" className="App-logo" alt="logo" />
       </header>
     </div>
   );
